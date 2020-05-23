f3dffd323a6772ba54ff293b8caa50be60e61bae
Rory Duncan <robust.rory@gmail.com>
2020-05-23 14:42:43 -0700

    Add dotenv and usage

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+import "dotenv-safe/config";
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
