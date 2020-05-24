cd95faa3573140be0013ca47bf61933b3098e348
Rory Duncan <robust.rory@gmail.com>
2020-05-23 17:21:36 -0700

    Startup firebase service on server start

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+// initialize firebase admin sdk
+import "./services/firebase.js";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
