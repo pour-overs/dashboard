1021bdedaa6ffac69a75025a15bb174f1d5d4081
Rory Duncan <robust.rory@gmail.com>
2020-05-25 14:32:02 -0700

    Add login-guard.js

---
+++ src/middleware/login-guard.js
@@ -0,0 +1,37 @@
+
+
+export default (redirectURL, publicURLs) => {
+
+  const isPublicURL = (url) => {
+    return publicURLs.filter((_url) => url.startsWith(_url)).length > 0;
+  }
+
+  return (req, res, next) => {
+
+    if (req.isAsset || isPublicURL(req.url)) {
+      return next();
+    }
+
+    if (!req.isAuthorized) {
+      return redirectToLogin(redirectURL, res);
+    }
+
+    next();
+  };
+}
+
+
+/**
+ * Helper for redirecting to a url ― particularly for not-authorized responses
+ * @param {Response} res
+ * @param {string} location
+ */
+const redirect = (redirectURL, res) => {
+  const str = `Redirecting to ${redirectURL}`;
+  res.writeHead(302, {
+      Location: redirectURL,
+      'Content-Type': 'text/plain',
+      'Content-Length': str.length,
+  });
+  res.end(str);
+}
