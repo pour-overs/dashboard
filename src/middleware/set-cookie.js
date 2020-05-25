a7e3957ca2b434e2052df53329595fae790932f5
Rory Duncan <robust.rory@gmail.com>
2020-05-25 11:30:52 -0700

    Add set-cookie middleware

---
+++ src/middleware/set-cookie.js
@@ -0,0 +1,20 @@
+import cookie from "cookie";
+
+/** Adds the cookie function for setting the `set-cookie` header on the response
+ * @middleware
+ * @param {HttpRequest} req The request object
+ * @param {HttpResponse} res The response object
+ * @param {Function} next A function to be called to continue the middleware chain
+ */
+export const setCookie = (req, res, next) => {
+  res.cookie = (key, value, options) => {
+    const cookieOptions = Object.assign({
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 5 // 5 days
+    }, options);
+    const setCookie = cookie.serialize(key, value, cookieOptions);
+    res.setHeader('Set-Cookie', setCookie);
+  };
+
+  next();
+}
