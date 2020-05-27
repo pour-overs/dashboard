5a097f84738621e999c60d7731c9f5150b99d109
Rory Duncan <robust.rory@gmail.com>
2020-05-26 22:36:15 -0700

    Add account section

---
+++ src/routes/account/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { getContext } from "svelte";
+  import UserCard from "../../components/UserCard.svelte";
+
+  let user = getContext("user");
+</script>
+
+<h1>Your Account</h1>
+<p>You're logged in as {user.name}.</p>
+
+<a href="/auth/login">Sign out</a>
+
+
