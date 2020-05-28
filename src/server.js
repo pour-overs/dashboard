d771e5c729963459f12231900219e4bb21f0e30b
Rory Duncan <robust.rory@gmail.com>
2020-05-28 16:33:33 -0700

    Add body-parser

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import helmet from 'helmet';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 // middlewares
 import setCookie from "./middleware/set-cookie.js";
@@ -14,6 +15,7 @@ import isAuthorized from "./middleware/is-authorized.js";
 import { isAsset } from "./middleware/is-asset.js";
 import loginGuard from "./middleware/login-guard.js";
 import responseJSON from "./middleware/response-json.js";
+
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -31,6 +33,7 @@ polka() // You can also use Express
     helmet(),
     cookieParser(),
     setCookie(),
+    bodyParser.json(),
     responseJSON(),
     isAsset("static"),
     isAuthorized(),
