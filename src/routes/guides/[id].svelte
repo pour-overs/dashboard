28d2081eddd89949b71021e5601726d0403c77d1
Rory Duncan <robust.rory@gmail.com>
2020-07-12 16:28:43 -0700

    Update form values to use a form object

--- src/routes/guides/[id].svelte
+++ src/routes/guides/[id].svelte
@@ -1,17 +1,17 @@
 <script context="module">
-
   export async function preload({ host, path, params, query }, session) {
     const { id } = params;
-    const response = await this.fetch(`/api/guides/${id}`, { credentials: "include", method: "GET", })
+    const response = await this.fetch(`/api/guides/${id}`, {
+      credentials: "include",
+      method: "GET"
+    });
     if (response.ok) {
       const guide = await response.json();
 
       console.log(guide);
       return { guide };
     }
-
   }
-
 </script>
 
 <script>
@@ -20,22 +20,28 @@
   let createdAt = new Date();
   createdAt.seconds = guide.createdAt._seconds;
 
-  // form values
-  let slug = guide.slug;
-  let description = guide.description;
-  let introductionContent = guide.introduction.content;
-  let youtubeUrl = guide.introduction.youtubeUrl;
-  let title = guide.title;
+  let hasChanged = false;
+
+  const form = {
+    slug: guide.slug || "",
+    title: guide.title || "",
+    description: guide.description || "",
+    introduction: {
+      content: guide.introduction.content || "",
+      youtubeUrl: guide.introduction.youtubeUrl || ""
+    }
+  };
 
   function save(e) {
+    console.log("Save form with", form);
+  }
 
+  function hasFormChanged() {
+    hasChanged = true;
   }
 </script>
 
 <style>
-
-
-
   label {
     display: block;
     margin: 0em auto;
@@ -57,57 +63,76 @@
     max-width: 120em;
   }
 
+  .actions {
+    display: grid;
+    grid-template-columns: 1fr auto auto auto;
+    grid-column-gap: 0.5em;
+    position: sticky;
+    padding: 1em 1em;
+    border: 1px solid #eee;
+    margin: 1em auto;
+    bottom: 0px;
+    left: 0px;
+    background-color: #fff;
+    border-radius: 3px;
+  }
 </style>
 
 <h1>Edit Guide</h1>
 
-<p>
-  Created on {createdAt.toLocaleDateString()}.
-</p>
+<p>Created on {createdAt.toLocaleDateString()}.</p>
 
-<form>
+<form on:input={hasFormChanged} on:reset={() => (hasChanged = false)}>
 
-  <div class="actions">
-    <button type="button">{guide.isPublished ? "Publish" : "Unpublish"}</button>
+  <div class="form-content">
+
+    <h2>Meta</h2>
+
+    <section class="form-group">
+      <label>
+        Title
+        <input type="text" bind:value={form.title} />
+      </label>
+
+      <label>
+        Short Description
+        <textarea rows="5" bind:value={form.description} />
+      </label>
+
+      <label>
+        Slug
+        <input type="text" bind:value={form.slug} />
+      </label>
+    </section>
+
+    <h2>Introduction</h2>
+    <section class="form-group">
+
+      <label>
+        Introductory Text
+        <textarea rows="8" bind:value={form.introduction.content} />
+      </label>
+
+      <label>
+        Youtube URL
+        <input type="text" bind:value={form.introduction.youtubeUrl} />
+      </label>
+    </section>
+
+    <section class="form-group">
+      <h2>Steps</h2>
+
+      <p>There are {guide.steps ? guide.steps : 0} steps.</p>
+    </section>
+
   </div>
 
-  <h2>Meta</h2>
 
-  <section class="form-group">
-    <label>
-      Title
-      <input type="text" bind:value={title} />
-    </label>
-
-    <label>
-      Short Description
-      <textarea rows="5" bind:value={description}></textarea>
-    </label>
-
-    <label>
-      Slug <input type="text" bind:value={slug} />
-    </label>
-  </section>
-
-  <h2>Introduction</h2>
-<section class="form-group">
-
-    <label>
-      Introductory Text
-      <textarea rows="8" bind:value={introductionContent}></textarea>
-    </label>
-
-    <label>
-      Youtube URL
-      <input type="text" bind:value={youtubeUrl} />
-    </label>
-</section>
-
-  <h2>Steps</h2>
-
-  <p>
-    There are {guide.steps ? guide.steps : 0} steps.
-  </p>
+  <footer class="actions">
+    <h2>{form.title || "Untitled"}</h2>
+    <button type="button">{guide.isPublished ? 'Unpublish' : 'Publish'}</button>
+    <button type="button" disabled={!hasChanged} on:click={save}>Save</button>
+    <button type="reset" disabled={!hasChanged}>Reset</button>
+  </footer>
 
 </form>
-
