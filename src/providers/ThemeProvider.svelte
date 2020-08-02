8e4ef79fd4ec75f16e29e7351505a93cc5fb5c8d
Rory Duncan <robust.rory@gmail.com>
2020-08-02 11:33:43 -0700

    transition theme changes

--- src/providers/ThemeProvider.svelte
+++ src/providers/ThemeProvider.svelte
@@ -4,6 +4,7 @@
 
   const LOCALSTORAGE_KEY = "theme";
   let firstMount = true;
+  let firstChange = true;
 
   onMount(() => {
 
@@ -16,10 +17,17 @@
     }
 
     if (document.body) {
-      const unlisten = theme.subscribe((theme) => {
+      const unlisten = theme.subscribe(async (theme) => {
+
+        if (!firstChange) {
+          document.body.classList.add("theme--animating");
+        }
+
         document.body.classList.toggle(THEMES.DEFAULT, theme === THEMES.DEFAULT);
         document.body.classList.toggle(THEMES.DARK_MODE, theme === THEMES.DARK_MODE);
         window.localStorage.setItem(LOCALSTORAGE_KEY, theme);
+
+        firstChange = false;
       });
 
       return unlisten;
