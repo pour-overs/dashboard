447bfd7f822eefc255bd2e996ddc9bf4df3e5ea0
Rory Duncan <robust.rory@gmail.com>
2020-08-02 12:27:18 -0700

    Remove lorem ipsum

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,17 +8,22 @@
 <PageTitle {title}>{title}</PageTitle>
 
 <p>
-	I'm baby disrupt knausgaard whatever banh mi pitchfork, roof party irony mixtape. Unicorn try-hard photo booth salvia, cray freegan hot chicken offal venmo locavore cloud bread cliche street art twee. Sartorial craft beer humblebrag, next level cardigan flexitarian tacos fixie banjo echo park typewriter shabby chic hexagon portland fam. Enamel pin banh mi hella, cronut glossier echo park wayfarers swag heirloom squid pork belly marfa. Artisan franzen vinyl bicycle rights distillery cornhole. Jean shorts photo booth cardigan craft beer.
+	This is the dashboard for the Pour Over Guides website, where guides can be created, edited, and published.
 </p>
 
-<p>
-	Disrupt forage normcore tbh asymmetrical 90's deep v man bun. Chia taiyaki squid chillwave kogi meditation truffaut letterpress iceland, cold-pressed raclette cronut lyft. Glossier cray squid, leggings banjo selvage kitsch. Taiyaki coloring book tacos scenester pok pok. Pabst put a bird on it iceland, la croix mumblecore meggings microdosing 3 wolf moon tumblr DIY VHS austin leggings green juice. Williamsburg yr migas microdosing cloud bread literally stumptown organic cold-pressed neutra keytar iceland deep v.
-</p>
+<p>The goal is to solve that there is no unifying source for learning about pour over guides on the web today.</p>
 
-<p>
-	Vaporware raw denim 90's, vape brunch asymmetrical hell of. Next level ramps letterpress, tote bag pok pok intelligentsia kombucha single-origin coffee. Bicycle rights banjo salvia affogato hoodie distillery. Gentrify lomo pok pok pickled meggings truffaut affogato vice umami. Kombucha photo booth PBR&B vice, asymmetrical mustache lumbersexual live-edge.
-</p>
+<p>Additionally, being able to compare, contrast, and glean interesting information from different methods.</p>
 
-<p>
-	Woke flannel polaroid plaid readymade bespoke schlitz yr asymmetrical palo santo next level. Viral locavore iPhone schlitz snackwave ennui ugh thundercats venmo hammock chartreuse lumbersexual you probably haven't heard of them subway tile fashion axe. Semiotics 3 wolf moon ugh narwhal drinking vinegar artisan venmo poke chicharrones. Health goth kogi wolf vexillologist mustache 3 wolf moon before they sold out raw denim kale chips hashtag echo park vice pug PBR&B tote bag. Brunch tbh succulents actually before they sold out. Dreamcatcher 3 wolf moon salvia try-hard marfa, tofu shoreditch adaptogen mixtape brooklyn.
-</p>
+
+<h3>Potential Additions</h3>
+
+<ul>
+	<li>
+		<a href="https://www.npmjs.com/package/svelte-frappe-charts">Add Charts</a>
+	</li>
+	<li>
+		Image uploads
+	</li>
+	<li>"Sponsered" guides?</li>
+</ul>
