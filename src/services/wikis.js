ff54a6f807479ead9c894af037f135334c4a4ece
Rory Duncan <robust.rory@gmail.com>
2021-01-02 22:30:56 -0800

    Add wiki service and api scaffold

---
+++ src/services/wikis.js
@@ -0,0 +1,20 @@
+import { firestore, firestoreFields } from "@services/firebase.js";
+
+const FieldValue = firestoreFields.FieldValue;
+const timestamp = () => FieldValue.serverTimestamp();
+
+
+const wikiRef = firestore.collection("wiki");
+
+
+export async function listWikis() {
+
+}
+
+export async function createWikiPage() {
+
+}
+
+export async function updateWikiPage() {
+
+}
