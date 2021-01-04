364c09b1b7099515f5d32aef329ad204f67a930c
Rory Duncan <robust.rory@gmail.com>
2021-01-03 22:26:20 -0800

    Fix duplicate deploys item

--- src/config/nav.config.js
+++ src/config/nav.config.js
@@ -6,7 +6,6 @@ export default [
     navigationLink("Guides", "library_books", "guides"),
     navigationLink("Wiki", "bookmarks", "wiki"),
     navigationLink("Deploys", "cloud_done", "deploys"),
-    navigationLink("Deploys", "cloud_done", "deploys"),
     navigationLink("Assets", "photo_library", "assets"),
     navigationLink("Users", "people", "users"),
     navigationLink("Account", "account_box", "account"),
